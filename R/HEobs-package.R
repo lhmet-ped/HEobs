a8582692ee5d1b92a72f790595b9a4314f22fe13
lhmet <lhmet@ufsm.br>
2020-10-23 20:28:32 -0300

    initial commit

---
+++ R/HEobs-package.R
@@ -0,0 +1,25 @@
+#' HEobs: Data Processing Of Natural Streamflow And Metadata From Ons Stations
+#'
+#'The goal of \pkg{HEgis} is to Ingest, clean and tidy data of natural
+#'streamflow and metadata from ONS stations.
+
+#'@section HEobs functions:
+#'
+#' \itemize{
+#'    \code{\link{import_qnat}}: Import data file of daily naturalized streamflow
+#'
+#'    \code{\link{extract_metadata}}: Import metadata from streamflow stations of ONS
+#'
+#'    \code{\link{wider}}: Pivot data from long to wide format.
+#'
+#'
+#'@docType package
+#'@name HEgis
+#'
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+
