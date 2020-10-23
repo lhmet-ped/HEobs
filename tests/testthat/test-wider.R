22913278ae2681f217347bdc890fb9000e610298
lhmet <lhmet@ufsm.br>
2020-10-23 20:55:50 -0300

    remove incomplete test

--- tests/testthat/test-wider.R
+++ tests/testthat/test-wider.R
@@ -1,11 +1,11 @@
-context("wider()")
-
-test_that("Produces the correct output", {
-
-  qnat <- import_qnat(wise_select(), TRUE, TRUE)
-  qnat_w <- wider(qnat)
-
-  qnat <- import_qnat(wise_select(), TRUE, FALSE)
-  qnat_w <- wider(qnat)
-
-  })
+# context("wider()")
+#
+# test_that("Produces the correct output", {
+#
+#   qnat <- import_qnat(wise_select(), TRUE, TRUE)
+#   qnat_w <- wider(qnat)
+#
+#   qnat <- import_qnat(wise_select(), TRUE, FALSE)
+#   qnat_w <- wider(qnat)
+#
+#   })
